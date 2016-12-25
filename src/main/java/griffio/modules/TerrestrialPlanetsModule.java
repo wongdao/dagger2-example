d65fe6e518374405c2f812d104f5f3dec4d9f758
DaoWong <ihuangtao@gmail.com>
2016-12-25 11:13:33 +0800

    add comments for Terrestrial Planets Module

--- src/main/java/griffio/modules/TerrestrialPlanetsModule.java
+++ src/main/java/griffio/modules/TerrestrialPlanetsModule.java
@@ -11,6 +11,10 @@ import griffio.planets.Venus;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
+/**
+ * 类地行星: 又称地球型行星（telluric planet）或岩石行星（rocky planet）都是指以硅酸盐岩石为主要成分的行星。
+ * 地球所在的太阳系有四颗类地行星：水星、金星、地球和火星，和一颗类地矮行星，谷神星。
+ */
 @Module
 public class TerrestrialPlanetsModule {
 
